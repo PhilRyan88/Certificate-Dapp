4a1df728a94e6d405555f6840d71d977a3cc45fa
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-09-05 22:20:26 +0530

    initialize

--- scripts/app.js
+++ scripts/app.js
@@ -9,6 +9,45 @@ let MyContract;
 window.onload = async () => {
   const abi = [
     {
+      inputs: [],
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      name: "certificates",
+      outputs: [
+        {
+          internalType: "string",
+          name: "name",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "course",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "date",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "Grade",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
       inputs: [
         {
           internalType: "uint256",
@@ -27,82 +66,18 @@ window.onload = async () => {
         },
         {
           internalType: "string",
-          name: "_grade",
+          name: "_date",
           type: "string",
         },
         {
           internalType: "string",
-          name: "_date",
+          name: "_Grade",
           type: "string",
         },
       ],
       name: "issue",
       outputs: [],
       stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [],
-      stateMutability: "nonpayable",
-      type: "constructor",
-    },
-    {
-      anonymous: false,
-      inputs: [
-        {
-          indexed: true,
-          internalType: "string",
-          name: "course",
-          type: "string",
-        },
-        {
-          indexed: false,
-          internalType: "uint256",
-          name: "id",
-          type: "uint256",
-        },
-        {
-          indexed: false,
-          internalType: "string",
-          name: "grade",
-          type: "string",
-        },
-      ],
-      name: "Issued",
-      type: "event",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      name: "Certificates",
-      outputs: [
-        {
-          internalType: "string",
-          name: "name",
-          type: "string",
-        },
-        {
-          internalType: "string",
-          name: "course",
-          type: "string",
-        },
-        {
-          internalType: "string",
-          name: "grade",
-          type: "string",
-        },
-        {
-          internalType: "string",
-          name: "date",
-          type: "string",
-        },
-      ],
-      stateMutability: "view",
       type: "function",
     },
   ];
