2cf0244022958558772adf1b9f8ffe690cc4bfec
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-09-05 22:07:36 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,116 @@
-import abi from "./Cert.json";
-
 connectToMetaMask = async () => {
   let accounts = await ethereum.request({ method: "eth_requestAccounts" });
   alert(`Connected to ${accounts[0]}!`);
 };
 
 let web3;
-let contractInstance;
-window.onload = () => {
+let MyContract;
+
+window.onload = async () => {
+  const abi = [
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "_id",
+          type: "uint256",
+        },
+        {
+          internalType: "string",
+          name: "_name",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "_course",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "_grade",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "_date",
+          type: "string",
+        },
+      ],
+      name: "issue",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "string",
+          name: "course",
+          type: "string",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "id",
+          type: "uint256",
+        },
+        {
+          indexed: false,
+          internalType: "string",
+          name: "grade",
+          type: "string",
+        },
+      ],
+      name: "Issued",
+      type: "event",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      name: "Certificates",
+      outputs: [
+        {
+          internalType: "string",
+          name: "name",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "course",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "grade",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "date",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
+  const contractAddress = "0x8Dcd577D68C9851bda157Eb7f26d5ab2D05834f1";
   web3 = new Web3(ethereum);
-  let ContractABI = abi;
-  let contractAddress = "0xc98510408A45805e3d5A9Efb29e851cc61D54751";
-  let contractInstance = new web3.eth.Contract(ContractABI, contractAddress);
+  MyContract = new web3.eth.Contract(abi, contractAddress);
 };
+
 issueCertificate = async () => {
   let certificateID = document.getElementById("certificateID").value;
   let candidateName = document.getElementById("candidateName").value;
